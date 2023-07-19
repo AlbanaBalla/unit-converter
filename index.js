d9689dbc59cb6f9c13a0b0b6672e4d07fa0f5e11
AlbanaBalla <138111535+AlbanaBalla@users.noreply.github.com>
2023-07-19 13:25:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+let unit = document.getElementById("unit")
+let convertbtn = document.getElementById("convert-btn")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
 
+convertbtn.addEventListener("click", function() {
+    const meterFeet = 3.281.toFixed(2)
+    const literGallon = 0.264
+    const kilogramPound = 2.204   
+      
+    let values = unit.value
+    
+    lengthEl.textContent = `${ values }  meter = ${values * meterFeet} feet | ${values} feet = ${(values / meterFeet).toFixed(2)} meter`
+    
+    volumeEl.textContent = `${ values } liter = ${values * literGallon} gallons | ${values} gallons = ${(values / literGallon).toFixed(2)} liter`
+    
+    massEl.textContent = `${ values } kilograms = ${values * literGallon} pounds | ${values} pounds = ${(values / literGallon).toFixed(2)} kilograms`
+    // lengthEl.textContent = `${values} feet = ${values / meterFeet} meter`
+})
